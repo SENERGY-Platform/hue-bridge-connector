6a3e3bc1412dc4f02deecfb8415f54c5b1c8b13b
y-du <42994541+y-du@users.noreply.github.com>
2018-09-10 11:22:05 +0200

    added config parser

--- hue_bridge/configuration.py
+++ hue_bridge/configuration.py
@@ -1,10 +1,50 @@
 if __name__ == '__main__':
     exit('Please use "client.py"')
 
+import os, configparser
 
-BRIDGE_HOST = '192.168.188.22'
-BRIDGE_PORT = 80
-BRIDGE_API_PATH = 'api'
-BRIDGE_API_KEY = 'yMCsyoJGmKBr2dRggCO-ANLIMoPEQ0T05qGi5Try'
+conf_path = '{}/hue_bridge'.format(os.getcwd())
+conf_file = 'bridge.conf'
 
-# add local config parser
+config = configparser.ConfigParser()
+
+
+if not os.path.isfile(os.path.join(conf_path, conf_file)):
+    print('No config file found')
+    config['BRIDGE'] = {
+        'host': '',
+        'port': '80',
+        'api_path': 'api',
+        'api_key': ''
+    }
+    config['SEPL'] = {
+        'device_type': ''
+    }
+    with open(os.path.join(conf_path, conf_file), 'w') as cf:
+        config.write(cf)
+    exit("Created blank config file at '{}'".format(conf_path))
+
+
+try:
+    config.read(os.path.join(conf_path, conf_file))
+except Exception as ex:
+    exit(ex)
+
+
+BRIDGE_HOST = config['BRIDGE']['host']
+BRIDGE_PORT = config['BRIDGE']['port']
+BRIDGE_API_PATH = config['BRIDGE']['api_path']
+BRIDGE_API_KEY = config['BRIDGE']['api_key']
+SEPL_DEVICE_TYPE = config['SEPL']['device_type']
+
+if not BRIDGE_HOST or not BRIDGE_PORT:
+    exit('Please provide Hue Bridge host and port')
+
+if not BRIDGE_API_PATH:
+    exit('Please provide Hue Bridge API path')
+
+if not BRIDGE_API_KEY:
+    exit('Please provide Hue Bridge API key')
+
+if not SEPL_DEVICE_TYPE:
+    exit('Please provide a SEPL device type')
