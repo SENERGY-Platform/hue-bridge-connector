53abd5a8675d19282c66f965d6edf3d14a8d5ff1
y-du <42994541+y-du@users.noreply.github.com>
2019-05-17 11:23:34 +0200

    add dictionary with mapping of log levels to human readable form, set logging level via config

--- hue_bridge/logger.py
+++ hue_bridge/logger.py
@@ -18,6 +18,14 @@ from .configuration import config
 import logging, cc_lib
 
 
-root_logger = logging.getLogger("hue-bridge-connector")
-root_logger.setLevel(logging.INFO)
-root_logger.addHandler(connector_lib_log_handler)
+logging_levels = {
+    'info': logging.INFO,
+    'warning': logging.WARNING,
+    'error': logging.ERROR,
+    'critical': logging.CRITICAL,
+    'debug': logging.DEBUG
+}
+
+
+root_logger = cc_lib.logger.getLogger("hue-bridge")
+root_logger.setLevel(logging_levels.setdefault(config.Logger.level, logging.INFO))
