e5f0151f1e6cedb5f2cdc817bf24726ce8bfd5f0
y-du <42994541+y-du@users.noreply.github.com>
2019-08-22 10:39:45 +0200

    Revert "add response error message"
    This reverts commit 412295bf4c8a56ecc7a09ac012928d1e75f8e865.

--- hue_bridge/types/service.py
+++ hue_bridge/types/service.py
@@ -64,7 +64,7 @@ def hueBridgePut(d_number: str, data: dict):
             else:
                 return True, "unknown error"
         else:
-            return True, "{} {}".format(resp.status_code, resp.text)
+            return True, resp.status_code
     except exceptions.RequestException:
         return True, "could not send request to hue bridge"
 
